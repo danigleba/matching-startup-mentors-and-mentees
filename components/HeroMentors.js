c4b178bb88965a8de6f69122c9ec4e8862fd8450
Dani Gleba <daniglebapuig@gmail.com>
2024-01-23 09:18:09 +0100

    fix: line-height in mentor's hero

--- components/HeroMentors.js
+++ components/HeroMentors.js
@@ -11,10 +11,8 @@ export default function Hero() {
         <div className="hero bg-[#f4f4f4] py-48 lg:py-0 lg:h-screen">
             <div className="mx-8 md:ml-20 md:mr-10">
                 <div>
-                    <p className="font-medium md:text-lg text-center lg:text-left text-[#222222]">Mentor an entrepreneur on the come-up and </p>
-                    <h1 className="z-30 text-3xl md:text-5xl">Turn your knoledge into<a className="bg-[#52b788] text-white px-2">cash</a></h1>
-                    <p className="text-[#222222] text-center lg:text-left md:text-lg">Get equity in early-stage startups by mentoring their founders. Help them get Product-Market-Fit and turn your experience into an asset.
-</p>
+                    <p className="font-medium md:text-lg text-center lg:text-left text-[#222222]">Mentor an entrepreneur on the come-up and</p>
+                    <h1 className="z-50 text-3xl md:text-5xl" style={{ lineHeight: "1.1" }}>Turn your knowledge into <a className="bg-[#52b788] text-white px-2">cash</a></h1>                    <p className="text-[#222222] text-center lg:text-left md:text-lg">Get equity in early-stage startups by mentoring their founders. Help them get Product-Market-Fit and turn your experience into an asset.</p>
                 </div>
                 <div className="md:flex justify-center lg:justify-start items-end gap-6 w-full pt-12">
                     <Link href="https://pqqg8ji8pbd.typeform.com/to/iYIo8V9o" target="_blank">
